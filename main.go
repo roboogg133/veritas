c7f4feca85e302270a87cd764f9084271a148e34
root <root@servidordomal.fun>
2025-05-24 20:02:01 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
-import(
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.Get("http://localhost:1143")
-	
+	prompt := "Qual a cor do céu?"
+
+	payload := []byte(fmt.Sprintf(`{"model": "gemma3:1b",
+		"prompt" : "%s",
+		"stream": false}`, prompt))
+
+	body := bytes.NewBuffer(payload)
+
+	response, err := http.Post("http://localhost:11434/api/generate", "application/json", body)
+	if err != nil {
+		log.Fatal("error doing POST request : ", err)
+	}
+
+	responseBody, err := io.ReadAll(response.Body)
+
+	if response.StatusCode == 200 {
+		log.Println(string(responseBody))
+	}
 }
